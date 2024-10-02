56dfa0ec8b990c241d005f39f08a4d3d258c2476
Vinícius Monteiro <vinicius3_1998@hotmail.com>
2024-10-02 10:51:03 -0300

    ajustes iniciais

--- src/App.java
+++ src/App.java
@@ -1,13 +1,13 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        Baralho baralho = new Baralho();
+        // Baralho baralho = new Baralho();
 
         // baralho.imprimeBaralho();
-        baralho.embaralha();
-        // baralho.imprimeBaralho();
-        System.out.println(baralho.distribuirCartas());
-        System.out.println(baralho.distribuirCartas());
-        System.out.println(baralho.distribuirCartas());
-        System.out.println(baralho.distribuirCartas());
+//         baralho.embaralha();
+//         // baralho.imprimeBaralho();
+//         System.out.println(baralho.distribuirCartas());
+//         System.out.println(baralho.distribuirCartas());
+//         System.out.println(baralho.distribuirCartas());
+//         System.out.println(baralho.distribuirCartas());
     }
 }
--- src/Baralho.java
+++ src/Baralho.java
@@ -1,54 +1,57 @@
+import java.util.ArrayList;
+
 public class Baralho {
-    public Carta[] baralho = new Carta[56];
+    public ArrayList<Carta> listaDeCartas = new ArrayList<>();
     public String[] cartas = {"Ás", "2", "3", "4", "5", "6", "7", "8", "9", "10", "dama", "valete", "reis", "coringa"};
     public String[] naipe = {"ouros", "espadilha", "copas", "paus"};
     public int posicao;
 
     public Baralho(){
-        int index = 0;
-
         for (int i=0; i < cartas.length ; i++){
             for (int j=0; j < naipe.length; j++){
                 Carta carta = new Carta(cartas[i], naipe[j]);
-                baralho[index] = carta;
+                listaDeCartas.add(carta);
 
-                if (carta.getCarta() == "coringa"){
+                if (carta.getCarta().equals("coringa")){
                     Carta cartaCoringa = new Carta("coringa", "coringa");
-                    baralho[index] = cartaCoringa;
+                    listaDeCartas.add(cartaCoringa);
                 }
-
-                index++;
             }
         }
     }
 
     public void embaralha(){
-        int qtdCartasNoBaralho = baralho.length;
+        int qtdCartasBaralho = listaDeCartas.size();
 
-        for (int i=0; i < qtdCartasNoBaralho; i++){
-            int posicaoAleatoriaDaCarta = (int) (Math.random() * qtdCartasNoBaralho);
+        for (int i = 0; i < qtdCartasBaralho; i++){
+            int posicaoAleatoriaDaCarta = (int) (Math.random() * qtdCartasBaralho);
 
-            Carta cartaAtual = baralho[i];
-            baralho[i] = baralho[posicaoAleatoriaDaCarta];
-            baralho[posicaoAleatoriaDaCarta] = cartaAtual;
+            Carta cartaAtual = listaDeCartas.get(i);
+            listaDeCartas.set(i, listaDeCartas.get(posicaoAleatoriaDaCarta));
+            listaDeCartas.set(posicaoAleatoriaDaCarta, cartaAtual);
         }
     }
 
-    public String distribuirCartas(){
-        Carta cartaAtual = baralho[posicao];
-        posicao++;
-
-        return cartaAtual.getCarta() + " de " + cartaAtual.getNaipe();
+    public String distribui(){
+        Carta carta = listaDeCartas.get(posicao);
+        listaDeCartas.remove(posicao);
+        return "Toma para vc " + carta.getCarta().toString() + " de " + carta.getNaipe().toString();
     }
 
     public void imprimeBaralho(){
-        int qtdCartasNoBaralho = baralho.length;
-
-        for(int i = 0; i < qtdCartasNoBaralho; i++){
-            System.out.println(i);
-            String carta = baralho[i].getCarta().toString();
-            String naipe =  baralho[i].getNaipe().toString();
-            System.out.println(carta + " de " + naipe); 
+        for (Carta carta : listaDeCartas){
+            System.out.println(carta.getCarta().toString() + " de " + carta.getNaipe().toString());
         }
+    }
+
+    public static void main(String[] args) {
+        Baralho baralho = new Baralho();
+        System.out.println(baralho.distribui());
+        System.out.println(baralho.distribui());
+        System.out.println(baralho.distribui());
+        baralho.embaralha();
+        System.out.println(baralho.distribui());
+        baralho.imprimeBaralho();
+
     }
 }
